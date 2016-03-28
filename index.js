7aa42c24442e96e2cba38fd9043c91f78b1f91f6
Oleg-Pashchenko <oleh.pashchenko@gmail.com>
2016-03-28 15:30:45 +0000

    1. Второй набор комментариев к коду.

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: А этот файл логично назвать "main.js"
 // nodejs при подключении модуля ищет файл {module_name}/index.js
+// Он ищет тот файл, который указан в файле пакетов. Просто надо разобраться.
 
+// TODO: Вот это я хотел, чтобы локально называлось как-то попроще. Хотя, вопрос спорный.
 var clusterfck = require("clusterfck");
 var check = require('check-types');
-
 var q = require('q');
 
 module.exports = function(json, propertyName) {
     
+    // TODO: А в чем, кстати, разница в отличии от "[]"? Сделаем единообразно.
     var temporary = new Array();
     
     var clusters = [];
@@ -21,6 +23,7 @@ module.exports = function(json, propertyName) {
 
     // TODO: Это можно очень классно заменить на самовызывающуюся функцию
     // Читаю о самовызывающихс функциях
+    // В месте вызова можно написать что-то типа function (property) {} (json[i][propertyName]);
     var processProperty = function(property) {
         
         if (check.date(new Date(property))) {
@@ -32,11 +35,13 @@ module.exports = function(json, propertyName) {
         }
     };
 
+    // TODO: Здесь и везде слово пишется как "cluster". В именах проекта везде - тоже.
     var clastirization = function() {
         var kMeans = new clusterfck.Kmeans();
         clusters = kMeans.cluster(temporary);
     };
 
+    // TODO: А "process" - это что именно? Мы же делаем конкретное действие. Давай его назовем.
     var processClusters = function() {
         for (var i = 0; i < clusters.length; i++) {
             for (var j = 0; j < clusters[i].length; j++) {
@@ -49,6 +54,8 @@ module.exports = function(json, propertyName) {
 
     // TODO: Не используем сокращения.
     // Id или какое тут сокращение? 
+    // Да. Все верно.
+    // TODO: Теперь с сокращениями все хорошо. Но "ToJSON" - это опять слово, очевидное из контекста.
     var addClusterIdentityToJSON = function(clusterIdentity, value) {
         for (var i = 0; i < json.length; i++) {
             var data = processProperty(json[i][propertyName]);
--- test.js
+++ test.js
@@ -1,12 +1,14 @@
 var request = require("request");
 
 // Берем JSON по URL и кластеризуем его
+// TODO: Я имею ввиду, что мы можем сделать более приятное описание, которое будет давать понимание пользователю, что это за данные и зачем их кластеризуем. Это как маркетинговый текст.
 request("http://google-observer-1.herokuapp.com/api/event/list?kernelIdentifier=55ccc5376675e91100163ec7", function(error, response, body) {
     
     var json = JSON.parse(body);
 
     // TODO: А мы можем подключать это не как файл, а как модуль?
     // Можем, в будующем, так как сейчас в NPM нет этого мдуля. 
+    // Но наш код и так является модулем. Поэтому он теоретически должен подключать сам себя. Типа как "./". Хотя, это только предположение.
     var index = require("./index.js");
     index(json, "date").start().then(function(result) {
         console.log(result);
